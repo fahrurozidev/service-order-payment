d0e2f077b07b65577aacdc3c466b31aeffc0f9e4
fahrurozi <m.fahruu.rozii@gmail.com>
2023-02-02 19:58:01 +0700

    make endpoint get order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -8,6 +8,21 @@ use Illuminate\Http\Request;
 class OrderController extends Controller
 {
     //
+    public function index(Request $request)
+    {
+        $userId = $request->input('user_id');
+        $orders = Order::query();
+
+        $orders->when($userId, function ($query) use ($userId) {
+            return $query->where('user_id', '=', $userId);
+        });
+
+        return response()->json([
+            'status'=> 'success',
+            'data'=> $orders->get()
+        ]);
+    }
+
     public function create(Request $request)
     {
         $user = $request->input('user');
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/orders', [OrderController::class, 'create']);
+Route::get('/orders', [OrderController::class, 'index']);
